62d0b45a2df36fca69e8b0c75d85897afd14f39e
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2025-04-09 20:07:23 +0100

    added api to generate access tokens

--- blogapp/auth/views.py
+++ blogapp/auth/views.py
@@ -3,9 +3,12 @@ from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, NotFound, AuthenticationFailed
+from rest_framework import status
 
 from auth.serializers import UserLoginRequest
-from blogapp.security_utils import is_correct_password, generate_refresh_token
+from blogapp.custom_exceptions import CustomException, JWT_TOKEN_MISSING_MSG, JWT_TOKEN_MISSING_CODE
+from blogapp.security_utils import is_correct_password, generate_refresh_token, validate_and_return_user_id_jwt_token, \
+    generate_access_token
 from user.models import User
 
 
@@ -25,11 +28,15 @@ class AuthenticationView(ViewSet):
                 raise NotFound(detail="username doesn't exist")
             if not is_correct_password(db_hashed_pw=user_obj.password, entered_pw=claimed_password):
                 raise AuthenticationFailed(detail="wrong password")
-            resp = Response()
+            resp = Response(status=status.HTTP_200_OK)
             return generate_refresh_token(user_id=user_obj.id, response=resp)
         else:
             raise ValidationError(detail="invalid request body")
 
     @action(detail=False, methods=["post"], url_path="refresh-token")
     def handle_refresh_token(self, request: Request) -> Response:
-        pass
+        refresh_token = request.COOKIES.get("refresh_token")
+        if refresh_token:
+            user_id = validate_and_return_user_id_jwt_token(refresh_token)
+            return Response(generate_access_token(user_id), status=status.HTTP_200_OK)
+        raise CustomException(JWT_TOKEN_MISSING_MSG, JWT_TOKEN_MISSING_CODE)
--- blogapp/blogapp/security_utils.py
+++ blogapp/blogapp/security_utils.py
@@ -1,8 +1,11 @@
 import bcrypt
 from datetime import timedelta
 import jwt
+from jwt import ExpiredSignatureError, InvalidTokenError
 from rest_framework.response import Response
 
+from blogapp.custom_exceptions import CustomException, JWT_TOKEN_EXPIRED_MSG, JWT_TOKEN_EXPIRED_CODE, \
+    JWT_TOKEN_INVALID_MSG, JWT_TOKEN_INVALID_CODE, JWT_TOKEN_ID_NOT_PRESENT_MSG, JWT_TOKEN_ID_NOT_PRESENT_CODE
 from blogapp.utils import now
 from blogapp.config import app_config
 
@@ -24,7 +27,7 @@ def generate_refresh_token(user_id: int, response: Response) -> Response:
     expiry_time = now() + timedelta(minutes=app_config.get_value("REFRESH_TOKEN_EXPIRY_MINS"))
     payload = {
         "user_id": user_id,
-        "exp": expiry_time.strftime("%Y-%m-%d %H:%M:%S %Z")
+        "exp": expiry_time.timestamp()
     }
     encoded_jwt = jwt.encode(payload, app_config.get_value("JWT_SECRET_KEY"), algorithm="HS256")
     # refresh token is a http only cookie
@@ -39,7 +42,24 @@ def generate_access_token(user_id: int) -> dict:
     expiry_time = now() + timedelta(minutes=app_config.get_value("ACCESS_TOKEN_EXPIRY_MINS"))
     payload = {
         "user_id": user_id,
-        "exp": expiry_time.strftime("%Y-%m-%d %H:%M:%S %Z")
+        "exp": expiry_time.timestamp()
     }
     encoded_jwt = jwt.encode(payload, app_config.get_value("JWT_SECRET_KEY"), algorithm="HS256")
     return {"access_token": encoded_jwt}
+
+
+def validate_and_return_user_id_jwt_token(token: str) -> int:
+    """
+    validates the signature and exp of the given jwt token
+    """
+    # throws exception if token is invalid or expired
+    try:
+        payload = jwt.decode(token, app_config.get_value("JWT_SECRET_KEY"), algorithms=["HS256"])
+    except ExpiredSignatureError:
+        raise CustomException(JWT_TOKEN_EXPIRED_MSG, JWT_TOKEN_EXPIRED_CODE)
+    except InvalidTokenError:
+        print("token:", token)
+        raise CustomException(JWT_TOKEN_INVALID_MSG, JWT_TOKEN_INVALID_CODE)
+    if "user_id" not in payload:
+        raise CustomException(JWT_TOKEN_ID_NOT_PRESENT_MSG, JWT_TOKEN_ID_NOT_PRESENT_CODE)
+    return payload["user_id"]
